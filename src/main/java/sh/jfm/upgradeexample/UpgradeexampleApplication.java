bfe678f04ebe3c8db1014d4e7f921a4e146df814
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-03-03 09:38:37 -0600

    Spring Initializer @ 3.3.9
    Needs to be downgraded

---
+++ src/main/java/sh/jfm/upgradeexample/UpgradeexampleApplication.java
@@ -0,0 +1,13 @@
+package sh.jfm.upgradeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgradeexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpgradeexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sh/jfm/upgradeexample/UpgradeexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.jfm.upgradeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpgradeexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
