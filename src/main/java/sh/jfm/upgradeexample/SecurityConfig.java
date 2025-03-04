b606dd88b36153b1a8ae191bbaa6eb9e1e2f97d0
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-03-04 22:46:53 +0000

    Fix deprecated security config
    See https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter

--- src/main/java/sh/jfm/upgradeexample/SecurityConfig.java
+++ src/main/java/sh/jfm/upgradeexample/SecurityConfig.java
@@ -4,20 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Previously SecurityConfigs extended WebSecurityConfigurerAdapter now a SecurityFilterChain bean is used for
+ * configuration.
+ * <p>
+ * See <a href="https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter">Spring
+ * Security without WebSecurityConfigurerAdapter</a> for more details.
+ */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/message", "/db-messages").permitAll()
@@ -25,10 +32,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+        return http.build();
     }
 
     @Bean
-    @Override
     protected UserDetailsService userDetailsService() {
         UserDetails user = User.builder()
                 .username("admin")
