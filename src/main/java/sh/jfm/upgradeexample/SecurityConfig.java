108f7c8f432feabe9cca6035028d85931d5b815f
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-03-04 18:08:11 +0000

    Turn off security debugging by default
    Typically, turn this on when debugging and then back off when done. Newer versions of Spring make this easier to isolate only to tests.

--- src/main/java/sh/jfm/upgradeexample/SecurityConfig.java
+++ src/main/java/sh/jfm/upgradeexample/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
