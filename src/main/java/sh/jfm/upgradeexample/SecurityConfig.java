3918b5dc01a874c01f0be2a3244b3bfaab5397a4
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-03-04 22:54:58 +0000

    Upgrade to Spring Boot 3.4.3

--- src/main/java/sh/jfm/upgradeexample/SecurityConfig.java
+++ src/main/java/sh/jfm/upgradeexample/SecurityConfig.java
@@ -2,6 +2,7 @@ package sh.jfm.upgradeexample;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -23,15 +24,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    /**
+     * The previous antMatchers implementation was removed in favor of requestMatchers.
+     * <p>
+     * See <a href="https://docs.spring.io/spring-security/reference/5.8/migration/servlet/config.html#use-new-requestmatchers">
+     * Configuration Migrations - Use the new requestMatchers methods
+     * </a> for more details.
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/message", "/db-messages").permitAll()
-                .antMatchers("/secure-message").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/message", "/db-messages").permitAll()
+                        .requestMatchers("/secure-message").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
